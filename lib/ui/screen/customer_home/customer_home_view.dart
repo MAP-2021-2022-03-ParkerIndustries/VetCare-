7f2d118ea91cf575ce0c5fcdf62e92bb1393639c
amirul-syafiq <amirulsyafiq@graduate.utm.my>
2022-05-30 15:13:51 +0800

    Make grid menu for customer home page

--- lib/ui/screen/customer_home/customer_home_view.dart
+++ lib/ui/screen/customer_home/customer_home_view.dart
@@ -16,49 +16,76 @@ class CustomerHomeView extends StatefulWidget {
 }
 
 class _CustomerHomeViewState extends State<CustomerHomeView> {
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppTheme.primary,
-          title: SizedBox(
-            height: 35,
-            child: Row(
-              children: [
-                Image.asset("assets/veterinarian.png", fit: BoxFit.cover),
-                const SizedBox(width: 10),
-                const Text(
-                  "Customer Home Page",
-                  style: AppTheme.headline3,
-                ),
-                const SizedBox(width: 10),
-              ],
-            ),
-          ),
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              onPressed: () {
-                // Navigator.of(context).pushNamed(Routes.profileRoute);
-              },
-              icon: const Icon(Icons.notifications),
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(Routes.profileRoute);
-              },
-              icon: const Icon(Icons.person),
-            ),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            children: const [Text("Home")],
+        child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppTheme.primary,
+        title: SizedBox(
+          height: 35,
+          child: Row(
+            children: [
+              Image.asset("assets/veterinarian.png", fit: BoxFit.cover),
+              const SizedBox(width: 10),
+              const Text(
+                "Customer Home Page",
+                style: AppTheme.headline3,
+              ),
+              const SizedBox(width: 10),
+            ],
           ),
         ),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Navigator.of(context).pushNamed(Routes.profileRoute);
+            },
+            icon: const Icon(Icons.notifications),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(Routes.profileRoute);
+            },
+            icon: const Icon(Icons.person),
+          ),
+        ],
       ),
-    );
+      body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(4, (index) {
+            return Center(
+                child: index == 0
+
+                    //Register pet Grid
+                    ? Column(children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(Routes.registerPetRoute);
+                          },
+                          icon: Image.asset('assets/pets.jpg'),
+                          iconSize: 150,
+                        ),
+                        const Text('Register Pet'),
+                      ])
+
+                    //Make a booking grid
+                    : index == 1
+                        ? Column(children: [
+                            IconButton(
+                              onPressed: () {
+                                // Navigator.of(context)
+                                //     .pushNamed(Routes.registerPetRoute);
+                              },
+                              icon: Image.asset('assets/cat.png'),
+                              iconSize: 150,
+                            ),
+                            const Text('Make a booking'),
+                          ])
+                        : Text('Item $index'));
+          })),
+    ));
   }
 }
