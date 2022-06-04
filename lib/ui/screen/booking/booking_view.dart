3314513752022bfbbfd284ab677d6768f3f1599a
Az3HyperX <93169556+Az3HyperX@users.noreply.github.com>
2022-06-05 01:49:03 +0800

    Help

--- lib/ui/screen/booking/booking_view.dart
+++ lib/ui/screen/booking/booking_view.dart
@@ -8,29 +8,70 @@ class BookingView extends StatefulWidget {
       MaterialPageRoute(builder: (_) => const BookingView());
   const BookingView({Key? key}) : super(key: key);
 
+ 
+
   @override
   State<BookingView> createState() => _BookingViewState();
 }
 
 class _BookingViewState extends State<BookingView> {
+
+  final _formkey = GlobalKey<FormState>();
+  TextEditingController _controller= TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppTheme.primary,
-          title: Text("Booking Page"),
+      appBar: AppBar(
+        backgroundColor: AppTheme.primary,
+        title: Text("Booking Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10.0 ,bottom:10 ),
+        child: Container(
+          child: Form(
+            key: _formkey,
+            child: Row(
+              children: [
+                _buildVetNameTextField(),
+                _buildPetTypeField()
+              ],
+            ),
+          ),
         ),
-        body: Center(child: Form(child: Text("macibai"))));
-  }
-  Widget _buildVetNameTextField()
-  {
-    return TextFormField(
+      ),
       
-      
-      
-
     );
+  }
 
+  Widget _buildVetNameTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+    icon: Icon(Icons.person),
+    hintText: 'Enter you pet Name',
+    labelText: 'Pet Name',
+  ),
+
+      
+    );
   }
 }
+
+Widget _buildPetTypeField() {
+    return DropdownButton<String>(
+      
+      value: "Dog",
+      icon: const Icon(Icons.arrow_drop_down_outlined),
+      onChanged: (String? newValue) {
+        
+      },
+      items: <String>['Dog', 'Cat', 'Rabbit', 'Bird']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
 
